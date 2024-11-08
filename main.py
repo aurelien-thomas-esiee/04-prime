e0ce3f4b17f0db105427662effad1771a0e1b640
aurelien-thomas-esiee <aurelien.thomas@edu.esiee.fr>
2024-11-08 13:55:58 +0000

    vérifie si un entier est premier ou non

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+"""Retourne si un nombre est premier ou non"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
+    """une fonction secondaire qui vérifie si un entier est un nombre premier ou pas."""
     # votre code ici
 
-    pass
+    if p<=1:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """la fonction principale qui fait appel à isprime et vérifie son bon fonctionnement"""
 
     # vos appels à la fonction secondaire ici
 
